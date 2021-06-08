083ea7e179a458b4f0e204d7a804eb7ac8d3fbee
coldlombax <coldlombax@gmail.com>
2021-06-08 10:15:41 +0100

    Planet component added.

---
+++ src/components/Navbar/Planet.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Planet({ name }) {
+  return <div></div>;
+}
+
+export default Planet;
+
+Planet.propTypes = {
+  name: PropTypes.string,
+};

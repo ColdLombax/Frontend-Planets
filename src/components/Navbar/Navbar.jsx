32dd5a0564e6d2d7340724f8de8fac8e04e89aef
coldlombax <coldlombax@gmail.com>
2021-06-08 09:33:59 +0100

    Navbar redone, cleaner code.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,26 @@
-import { motion } from "framer-motion";
 import PropTypes from "prop-types";
 import { useState } from "react";
 
 import hamburger from "../../assets/icon-hamburger.svg";
+import NavItem from "./NavItem";
 
 function Navbar({ data }) {
   const [isActive, setIsActive] = useState(true);
   const toggleMenu = () => {
     setIsActive(!isActive);
   };
+  const navItems = data.map((planet) => {
+    return <NavItem key={planet.name} name={planet.name} />;
+  });
+
   return (
     <nav className='navbar'>
       <h1>THE PLANETS</h1>
       <img src={hamburger} alt='menu toggle' onClick={toggleMenu} />
       {isActive && (
-        <motion.div className='dropdown'>
-          <p>Hello</p>
-        </motion.div>
+        <div className='dropdown'>
+          <ul className='nav-items'>{navItems}</ul>
+        </div>
       )}
     </nav>
   );

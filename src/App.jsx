ed323d80046ae78d5ab8420a7b56cd5fbc9ca1b6
coldlombax <coldlombax@gmail.com>
2021-06-08 10:09:01 +0100

    Added new details navigation section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import DetailsNav from "./components/Navbar/DetailsNav";
 
 import Navbar from "./components/Navbar/Navbar";
 
 import data from "./data.json";
 
 function App() {
+  const [currentPlanet, setCurrentPlanet] = useState("mercury");
+
   return (
     <div className='App'>
-      <Navbar data={data} />
+      <Navbar data={data} setPlanet={setCurrentPlanet} />
+      <section className='details-nav'>
+        <DetailsNav name={currentPlanet} />
+      </section>
     </div>
   );
 }

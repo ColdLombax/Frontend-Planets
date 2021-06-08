75d2e7416fba4aa6f0696e56eb8ea3d741ff931a
coldlombax <coldlombax@gmail.com>
2021-06-08 11:55:46 +0100

    Planet component added with individual states.

--- src/components/Navbar/Planet.jsx
+++ src/components/Navbar/Planet.jsx
@@ -1,12 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-function Planet({ name }) {
-  return <div></div>;
+import PlanetFact from "./PlanetFact";
+
+function Planet({ currentPlanet, data }) {
+  // Details
+  const [overview, setOverview] = useState("");
+  const [overviewSource, setOverviewSource] = useState("");
+
+  // Facts
+  const [rotationTime, setRotationTime] = useState("");
+  const [revolutionTime, setRevolutionTime] = useState("");
+  const [radius, setRadius] = useState("");
+  const [avarageTemp, setAvarageTemp] = useState("");
+
+  const getDetails = () => {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].name.toLowerCase() === currentPlanet) {
+        // Overview Assignment
+        setOverview(data[i].overview.content);
+        setOverviewSource(data[i].overview.source);
+
+        // Fact Assignment
+        setRotationTime(data[i].rotation);
+        setRevolutionTime(data[i].revolution);
+        setRadius(data[i].radius);
+        setAvarageTemp(data[i].temperature);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getDetails();
+  }, [currentPlanet]);
+
+  return (
+    <div className='planet-container'>
+      <div className={`planet ${currentPlanet}-planet`}></div>
+      <div className='planet-details'>
+        <h2>{currentPlanet.toUpperCase()}</h2>
+        <p>{overview}</p>
+        <p className='source-link'>
+          Source:{" "}
+          <a href={overviewSource} target='_blank' rel='noreferrer'>
+            Wikipedia
+          </a>
+        </p>
+        <div className='planet-facts'>
+          <PlanetFact title='rotation time' fact={rotationTime} />
+          <PlanetFact title='revolution time' fact={revolutionTime} />
+          <PlanetFact title='radius' fact={radius} />
+          <PlanetFact title='avarage temp.' fact={avarageTemp} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Planet;
 
 Planet.propTypes = {
-  name: PropTypes.string,
+  currentPlanet: PropTypes.string,
+  data: PropTypes.array,
 };

43b30cf5b9e7b1134468ea780192579fffaf2f1b
coldlombax <coldlombax@gmail.com>
2021-06-08 09:34:15 +0100

    NavItem cleaner with independent styling.

--- src/components/Navbar/NavItem.jsx
+++ src/components/Navbar/NavItem.jsx
@@ -4,7 +4,20 @@ import PropTypes from "prop-types";
 import chevron from "../../assets/icon-chevron.svg";
 
 function NavItem({ name }) {
-  return <li></li>;
+  return (
+    <li className='nav-item'>
+      <div
+        style={{
+          width: "1.5em",
+          height: "1.5em",
+          borderRadius: "100%",
+          backgroundColor: `var(--clr-${name.toLowerCase()})`,
+        }}
+      ></div>
+      <h3>{name.toUpperCase()}</h3>
+      <img src={chevron} alt='chevron indicator' width='10px' />
+    </li>
+  );
 }
 
 export default NavItem;
